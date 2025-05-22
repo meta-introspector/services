be683d870f44bddf53dfb9aec1a4e4848b0f5d3e
gd <gentdupont@gmail.com>
2025-05-22 18:33:49 -0400

    fix

--- scripts/setup-swap.sh
+++ scripts/setup-swap.sh
@@ -1,14 +1,17 @@
 
-dd if=/dev/zero of=/swapfile bs=128M count=32
-sudo chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+if [ ! -f /swapfile ]; then
+    dd if=/dev/zero of=/swapfile bs=128M count=32
+    sudo chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+fi
+
 swapon -s
 #To start the swap file at boot time, edit the /etc/fstab file. Open the file in the editor, and then run the following command:
 
 #$ sudo vi /etc/fstab
 #Add the following new line at the end of the file:
-if [ grep swapfile /etc/fstab ]; then
+if [ grep -q swapfile /etc/fstab ]; then
   echo "swapfile already exists"
 else
     echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
