ae771ef79b7f662a7932a6f11e521885d08b8c85
gd <gentdupont@gmail.com>
2025-05-23 16:06:40 -0400

    ok lets try it

--- scripts/setup-solana.sh
+++ scripts/setup-solana.sh
@@ -10,3 +10,9 @@ bash -x /opt/services/scripts/setup-kernel.sh
 
 echo setup swap
 bash -x /opt/services/scripts/setup-swap.sh
+
+echo start solana
+bash -x /opt/services/scripts/start-solana.sh
+
+echo status solana
+bash -x /opt/services/scripts/status-solana.sh
